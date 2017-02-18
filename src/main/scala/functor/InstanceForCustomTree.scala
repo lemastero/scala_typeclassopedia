67a9bd4b931f65d780e991ba2cd0044af20f5e2a
lemastero <adres.mistrza@gmail.com>
2017-02-18 20:10:27 +0100

    functor instance implementation

--- src/main/scala/functor/InstanceForCustomTree.scala
+++ src/main/scala/functor/InstanceForCustomTree.scala
@@ -8,7 +8,10 @@ final case class Branch[T](left: Tree[T], right: Tree[T]) extends Tree[T]
 
 object Tree {
   val treeFunctor = new Functor[Tree] {
-    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = ???
+    override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] =  fa match {
+      case Leaf(v) => Leaf(f(v))
+      case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+    }
   }
 }
 
