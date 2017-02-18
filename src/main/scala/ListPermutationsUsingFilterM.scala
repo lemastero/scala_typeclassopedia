30368b9a51addf6c43c5f03c6358901bde6320f9
lemastero <adres.mistrza@gmail.com>
2017-02-18 16:11:58 +0100

    add name of the blogger to docs

--- src/main/scala/ListPermutationsUsingFilterM.scala
+++ src/main/scala/ListPermutationsUsingFilterM.scala
@@ -8,7 +8,7 @@ import cats.implicits._
   *
   * Explanations:
   *   http://stackoverflow.com/questions/28872396/haskells-filterm-with-filterm-x-true-false-1-2-3
-  *   (Haskell) https://byorgey.wordpress.com/2007/06/26/deducing-code-from-types-filterm/
+  *   (Brent Yorgey in Haskell) https://byorgey.wordpress.com/2007/06/26/deducing-code-from-types-filterm/
   */
 object ListPermutationsUsingFilterM extends App {
   val res: List[List[Int]] = List(1, 2, 3).filterA(_ => List(true, false))
