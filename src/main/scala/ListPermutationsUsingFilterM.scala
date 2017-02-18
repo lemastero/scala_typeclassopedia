89bdf7d27c658798fbb0d8b7f2700e4f736ae960
lemastero <adres.mistrza@gmail.com>
2017-02-18 16:09:07 +0100

    gnerate list permutations using filterA from TraverseFilter

---
+++ src/main/scala/ListPermutationsUsingFilterM.scala
@@ -0,0 +1,16 @@
+
+import cats.implicits._
+
+/**
+  * Generate all permutations of the list using filterM
+  *
+  * MonadFilter filterM moved to TraverseFilter filterA: https://github.com/typelevel/cats/blob/master/CHANGES.md
+  *
+  * Explanations:
+  *   http://stackoverflow.com/questions/28872396/haskells-filterm-with-filterm-x-true-false-1-2-3
+  *   (Haskell) https://byorgey.wordpress.com/2007/06/26/deducing-code-from-types-filterm/
+  */
+object ListPermutationsUsingFilterM extends App {
+  val res: List[List[Int]] = List(1, 2, 3).filterA(_ => List(true, false))
+  println(res)
+}
