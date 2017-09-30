687a5fe0dda933e1d240dc8921490d51513f2536
lemastero <adres.mistrza@gmail.com>
2017-09-30 20:24:58 +0200

    move Tree implementation from functor package to helper_implementations

--- src/main/scala/helper_implementations/Tree.scala
+++ src/main/scala/helper_implementations/Tree.scala
@@ -1,4 +1,4 @@
-package functor
+package helper_implementations
 
 import cats.Functor
 
@@ -7,7 +7,7 @@ final case class Leaf[T](value: T) extends Tree[T]
 final case class Branch[T](left: Tree[T], right: Tree[T]) extends Tree[T]
 
 object Tree {
-  val treeFunctor = new Functor[Tree] {
+  val treeFunctor: Functor[Tree] = new Functor[Tree] {
     override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] =  fa match {
       case Leaf(v) => Leaf(f(v))
       case Branch(l, r) => Branch(map(l)(f), map(r)(f))
