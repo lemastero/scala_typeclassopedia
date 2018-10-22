5a016752188586a04cae9c187c656473bc7d874d
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2018-10-22 02:02:47 +0200

    full implementation of Day convolution

--- src/main/scala/day/DayConvolutionSimpleImpl.scala
+++ src/main/scala/day/DayConvolutionSimpleImpl.scala
@@ -3,12 +3,36 @@ package day
 import applicative.ApplicativeSimpleImpl.Applicative
 import comonad.ComonadSimpleImpl.Comonad
 import functor.FunctorSimpleImpl.Functor
+import natural_transformation.NaturalTransf.NaturalTransf
+import simple.Id
 
 import scala.language.higherKinds
 
 object DayConvolutionSimpleImpl {
 
-  /** Day convolution */
+  /** The Day convolution of two covariant functors.
+    *
+    * Day convolution provides a monoidal product.
+    *
+    * Identity functor is the unit of Day convolution on both sides:
+    * - intro1(elim1) = id
+    * - elim1(intro1) = id
+    *
+    * - intro2(elim2) = id
+    * - elim2(intro2) = id
+    *
+    * The monoid for Day convolution on the cartesian monoidal structure is symmetric:
+    * - map(f)(d.swapped) = swapped(map(f)(d))
+    *
+    * The associativity of the monoid is witnessed by 'assoc' and 'disassoc'
+    * - assoc . disassoc = id
+    * - disassoc . assoc = id
+    * - fmap f . assoc = assoc . fmap f
+    *
+    * Naturality of trans1 and trans2:
+    * - fmap f . trans1 fg = trans1 fg . fmap f
+    * - fmap f . trans2 fg = trans2 fg . fmap f
+    */
   trait Day[F[_], G[_], A] { self =>
     type X
     type Y
@@ -24,18 +48,103 @@ object DayConvolutionSimpleImpl {
         val gy: G[Y] = self.gy
         def xya: (X, Y) => B = (x, y) => f(self.xya(x, y))
       }
+
+    /** Swap type constructors order */
+    def swapped: Day[G, F, A] = new Day[G, F, A] {
+      type X = self.Y
+      type Y = self.X
+      val fx: G[X] = self.gy
+      val gy: F[Y] = self.fx
+      def xya: (X, Y) => A = (x, y) => self.xya(y, x)
+    }
+
+    /** Apply a natural transformation to the left-hand side of a Day convolution. */
+    def trans1[H[_]](nat: NaturalTransf[F, H]): Day[H, G, A] = new Day[H, G, A] {
+      type X = self.X
+      type Y = self.Y
+      val fx: H[X] = nat.apply(self.fx)
+      val gy: G[Y] = self.gy
+      def xya: (X, Y) => A = self.xya
+    }
+
+    /** Apply a natural transformation to the right-hand side of a Day convolution. */
+    def trans2[H[_]](nat: NaturalTransf[G, H]): Day[F, H, A] = new Day[F, H, A] {
+      type X = self.X
+      type Y = self.Y
+      val fx: F[X] = self.fx
+      val gy: H[Y] = nat.apply(self.gy)
+      def xya: (X, Y) => A = self.xya
+    }
   }
 
   object Day {
 
     /** Construct the Day convolution */
-    def apply[F[_], G[_], A, B](fab: F[A => B], ga: G[A]): Day[F, G, B] = new Day[F, G, B] {
-      type X = A=>B
+    def day[F[_], G[_], A, B](fab: F[A => B], ga: G[A]): Day[F, G, B] = new Day[F, G, B] {
+      type X = A => B
       type Y = A
       val fx: F[X] = fab
       val gy: G[Y] = ga
-      def xya: (X, Y) => B = (x,y) => x(y)
+      def xya: (X, Y) => B = (x, y) => x(y)
     }
+
+    def intro1[F[_], A](fa: F[A]): Day[Id, F, A] = new Day[Id, F, A] {
+      type X = Unit
+      type Y = A
+      val fx: Id[X] = Id(value = ())
+      val gy: F[Y] = fa
+      def xya: (X, Y) => A = (_, a) => a
+    }
+
+    def intro2[F[_], A](fa: F[A]): Day[F, Id, A] = new Day[F, Id, A] {
+      type X = A
+      type Y = Unit
+      val fx: F[X] = fa
+      val gy: Id[Y] = Id(value = ())
+      def xya: (X, Y) => A = (a, _) => a
+    }
+
+    /** Collapse to second type constructor if first one is Identity */
+    def elim1[F[_], A](d: Day[Id, F, A])(implicit FunF: Functor[F]): F[A] = FunF.map(d.gy)(d.xya(d.fx.value, _))
+
+    /** Collapse to first type constructor if second one is Identity */
+    def elim2[F[_], A](d: Day[F, Id, A])(implicit FunF: Functor[F]): F[A] = FunF.map(d.fx)(d.xya(_, d.gy.value))
+
+    /** Collapse to type constructor if both of them are the same */
+    def dap[F[_], A](d: Day[F, F, A])(implicit AF: Applicative[F]): F[A] = AF.liftA2(d.xya)(d.fx, d.gy)
+
+    def assoc[F[_], G[_], H[_], A, B](d: Day[F, Day[G, H, ?], A]): Day[Day[F, G, ?], H, A] = {
+      new Day[Day[F, G, ?], H, A] {
+        type X = (d.X, d.gy.X)
+        type Y = d.gy.Y
+        val fx: Day[F, G, (d.X, d.gy.X)] = new Day[F, G,(d.X, d.gy.X)] {
+          type X = d.X
+          type Y = d.gy.X
+          val fx: F[X] = d.fx
+          val gy: G[Y] = d.gy.fx
+          def xya: (X, Y) => (d.X, d.gy.X) = (x, y) => (x, y)
+        }
+        val gy: H[Y] = d.gy.gy
+        def xya: (X, Y) => A = (a, e) => d.xya(a._1, d.gy.xya(a._2, e))
+      }
+    }
+
+    def disassoc[F[_], G[_], H[_], A](d: Day[Day[F, G, ?], H, A]): Day[F, Day[G, H, ?], A] = new Day[F, Day[G, H, ?], A] {
+      type X = d.fx.X
+      type Y = (d.fx.Y, d.Y)
+      val fx: F[X] = d.fx.fx
+      val gy:  Day[G, H, (d.fx.Y, d.Y)] = new Day[G, H, (d.fx.Y, d.Y)] {
+        type X = d.fx.Y
+        type Y = d.Y
+        val fx: G[X] = d.fx.gy
+        val gy: H[Y] = d.gy
+        def xya: (X, Y) => (d.fx.Y, d.Y) = (x, y) => (x,y)
+      }
+      def xya: (X, Y) => A = (x: d.fx.X, y: (d.fx.Y, d.Y) ) => d.xya(d.fx.xya(x, y._1), y._2)
+    }
+  }
+
+  object DayInstances {
 
     /** Functor (for free) for Day convolution */
     def functorDay[F[_], G[_]]: Functor[Day[F, G, ?]] = new Functor[Day[F, G, ?]] {
@@ -70,8 +179,19 @@ object DayConvolutionSimpleImpl {
       new Comonad[Day[F, G, ?]] {
         def extract[C](w: Day[F, G, C]): C = w.xya(CF.extract(w.fx), CG.extract(w.gy))
 
-        def duplicate[C](wa: Day[F, G, C]): Day[F, G, Day[F, G, C]] = ???
-
+        def duplicate[C](wa: Day[F, G, C]): Day[F, G, Day[F, G, C]] = new Day[F, G, Day[F, G, C]] {
+          type X = F[wa.X]
+          type Y = G[wa.Y]
+          val fx: F[X] = CF.duplicate(wa.fx)
+          val gy: G[Y] = CG.duplicate(wa.gy)
+          def xya: (X, Y) => Day[F, G, C] = (x,y) => {new Day[F, G, C] {
+            type X = wa.X
+            type Y = wa.Y
+            val fx: F[X] = x
+            val gy: G[Y] = y
+            def xya: (X, Y) => C = (x,y) => wa.xya(x,y)
+          }}
+        }
         def map[C, D](d: Day[F, G, C])(f: C => D): Day[F, G, D] = d.map(f)
       }
   }
