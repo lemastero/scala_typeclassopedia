6325ef9df207af8927d601a410befb6597df423c
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2018-11-12 10:43:12 +0100

    split Divisible into Divide and Divisible

--- src/main/scala/contravariant/DivisibleSimpleImpl.scala
+++ src/main/scala/contravariant/DivisibleSimpleImpl.scala
@@ -6,8 +6,11 @@ import scala.language.higherKinds
 
 object DivisibleSimpleImpl {
 
-  trait Divisible[F[_]] extends Contravariant[F] {
+  trait Divide[F[_]] extends Contravariant[F] {
     def divide[A,B,C](f: A => (B,C), fb: F[B], fc: F[C]): F[A]
+  }
+
+  trait Divisible[F[_]] extends Divide[F] {
     def conquer[A]: F[A]
   }
 }
