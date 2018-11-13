ce8b4b88f8bf74212d24435e2d065ec0b3ba6f14
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2018-11-13 23:25:40 +0100

    split Decide and Decidable

--- src/main/scala/contravariant/DecidableSimpleImpl.scala
+++ src/main/scala/contravariant/DecidableSimpleImpl.scala
@@ -1,15 +1,17 @@
 package contravariant
 
 import contravariant.DivisibleSimpleImpl.Divisible
+import simple.Void
 
 import scala.language.higherKinds
 
 object DecidableSimpleImpl {
 
-  type Void <: Nothing
-
-  trait Decidable[F[_]] extends Divisible[F] {
+  trait Decide[F[_]] extends Divisible[F] {
     def choose[A,B,C](f: A => Either[B,C], fb: F[B], fc: F[C]): F[A]
-    def loose[A](f: A => Void)
+  }
+
+  trait Decidable[F[_]] extends Divisible[F] with Decide[F] {
+    def loose[A](f: A => Void): F[A]
   }
 }
