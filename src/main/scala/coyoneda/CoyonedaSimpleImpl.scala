4e160524f3afe3113f43f4cd43822f1f44cf5d63
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2018-11-15 01:54:10 +0100

    add Coyoneda implementation

---
+++ src/main/scala/coyoneda/CoyonedaSimpleImpl.scala
@@ -0,0 +1,65 @@
+package coyoneda
+
+import functor.FunctorSimpleImpl.Functor
+import kan.LeftKanExtensionSimpleImpl.Lan
+import natural_transformation.NaturalTransf.NaturalTransf
+import simple.Id
+
+import scala.language.higherKinds
+
+/*
+Functor with some a's in it is isomorphic to some other type
+that has function b -> a and some functor with b's inside
+
+ f a ~ forall b. ((b -> a), f b)
+ */
+trait Coyoneda[F[_], A] {
+  type B
+  def f: B => A
+  def fb: F[B]
+
+
+  /* If we have proof that type constructor F is a Functor - instance for Functor[F] then we can create F[A].
+  So we lower Coyoneda to Functor F. */
+  def lowerCoyoneda(implicit fun: Functor[F]): F[A] = fun.map(fb)(f)
+
+  /* Coyoneda is the Left Kan extension of F alogn the Identity functor */
+  // coyonedaToLan :: Coyoneda f a -> Lan Identity f a
+  def coyonedaToLan(): Lan[Id,F,A] = Lan[Id,F,A,B](fb, a => f(a.value))
+}
+
+object Coyoneda {
+
+  def apply[F[_],AA,BB](ff: BB => AA, ffb: F[BB]): Coyoneda[F,AA] = new Coyoneda[F,AA] {
+    type B = BB
+    def f: B => AA = ff
+    def fb: F[B] = ffb
+  }
+
+  /* lift any type constructor F to Coyoneda F A */
+  def liftCoyoneda[F[_], A](fa: F[A]): Coyoneda[F, A] = Coyoneda[F, A, A](identity[A], fa)
+
+  /*Instance of Functor for Coyoneda F */
+  implicit def coyoFunctor[F[_]]: Functor[Coyoneda[F, ?]] = new Functor[Coyoneda[F, ?]] {
+    def map[A, AA](fa: Coyoneda[F, A])(ff: A => AA): Coyoneda[F, AA] = new Coyoneda[F, AA] {
+      type B = fa.B
+      def f: B => AA = fa.f andThen ff
+      def fb: F[B] = fa.fb
+    }
+  }
+
+  /* lift natural transformation to natural transformation on Coyoneda */
+  def hoistCoyoneda[F[_], G[_], A, C](fab : NaturalTransf[F,G])(coyo: Coyoneda[F, A]): Coyoneda[G, A] =
+    new Coyoneda[G, A] {
+      type B = coyo.B
+      def f: B => A = coyo.f
+      def fb: G[B] = fab(coyo.fb)
+    }
+}
+
+object SimpleImpl extends App {
+  val s = Set(1, 2, 3, 4, 5)
+  val s2 = Coyoneda.liftCoyoneda(s)
+  val s3 = Coyoneda.coyoFunctor.map(s2)(_ % 2) // mapping over set
+  println(s3)
+}
