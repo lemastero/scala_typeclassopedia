d55452be6dc24e314b864f6eb35700876480cae5
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2019-01-13 16:59:04 +0100

    add Lan Functor instance using Scalaz style

--- src/main/scala/kan/LeftKanExtensionSimpleImpl.scala
+++ src/main/scala/kan/LeftKanExtensionSimpleImpl.scala
@@ -6,27 +6,39 @@ import scala.language.higherKinds
 
 object LeftKanExtensionSimpleImpl {
 
-  trait Lan[F[_], H[_], A] {
-    type B
-    val hb: H[B]
-    def f: F[B] => A
+  trait Lan[G[_], H[_], A] {
+    type I
+    val v: H[I]
+    def f: G[I] => A
   }
 
-  object Lan {
-    def apply[F[_], H[_], A, BB](hhb: H[BB], ff: F[BB] => A): Lan[F,H,A] = new Lan[F,H,A] {
-      type B = BB
-      val hb: H[B] = hhb
-      def f: F[B] => A = ff
+  object Lan extends LanInstances {
+    def apply[G[_], H[_], A, B](hhb: H[B], ff: G[B] => A): Lan[G,H,A] = new Lan[G,H,A] {
+      type I = B
+      val v: H[I] = hhb
+      def f: G[I] => A = ff
     }
+  }
 
-    def lanFunctor[F[_], H[_]]: Functor[Lan[F, H, ?]] = new Functor[Lan[F, H, ?]]() {
+  sealed abstract class LanInstances {
+    implicit def lanFunctor[F[_], H[_]]: Functor[Lan[F, H, ?]] = new Functor[Lan[F, H, ?]]() {
       def map[A, X](x: Lan[F, H, A])(fax: A => X): Lan[F, H, X] = {
         new Lan[F, H, X] {
-          type B = x.B
-          val hb: H[B] = x.hb
+          type I = x.I
+          val v: H[I] = x.v
 
-          def f: F[B] => X = x.f andThen fax
+          def f: F[I] => X = x.f andThen fax
         }
+      }
+    }
+  }
+
+  private trait LanFunctor[G[_], H[_]] extends Functor[Lan[G, H, ?]] {
+    def map[A, X](x: Lan[G, H, A])(fax: A => X): Lan[G, H, X] = {
+      new Lan[G, H, X] {
+        type I = x.I
+        val v: H[I] = x.v
+        def f: G[I] => X = x.f andThen fax
       }
     }
   }
