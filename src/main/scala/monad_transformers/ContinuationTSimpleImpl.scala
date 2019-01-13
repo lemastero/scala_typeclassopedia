b9bf48c9ef2d5db06c8b6249cbca66fa9f36825a
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2019-01-13 14:11:39 +0100

    add continuation monad

--- src/main/scala/monad_transformers/ContinuationTSimpleImpl.scala
+++ src/main/scala/monad_transformers/ContinuationTSimpleImpl.scala
@@ -1,8 +1,25 @@
 package monad_transformers
 
+import monad.MonadSimpleImplementation.Monad
+
 import scala.language.higherKinds
 
 object ContinuationTSimpleImpl {
+
+  /** Continutation monad */
+  trait Cont[M[_],A] {
+    def run[Z](f: A => M[Z]): M[Z]
+  }
+
+  def contInstance[M[_]]: Monad[Cont[M, ?]] = new Monad[Cont[M, ?]] {
+    def pure[A](a: A): Cont[M, A] = new Cont[M,A] {
+      def run[Z](f: A => M[Z]): M[Z] = f(a)
+    }
+
+    def map[A, B](x: Cont[M, A])(f: A => B): Cont[M, B] = new Cont[M, B] {
+      def run[Z](ff: B => M[Z]): M[Z] = x.run(f andThen ff)
+    }
+  }
 
   /** Continutation monad transformer */
   trait ContT[R, M[_], A] {
