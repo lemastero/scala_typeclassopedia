7aa9b903beb85645a1042c84f8fd7dab6ed88859
Piotr Paradzinski <piotr.paradzinski@scalac.io>
2019-01-18 01:26:21 +0100

    add CeyleyTheorem for Monoid (MonoidK) and description of Difference lists, short info about Double Cayley, improve Exponentails

---
+++ src/main/scala/cayley/CayleyTheorem.scala
@@ -0,0 +1,24 @@
+package cayley
+
+import cats.MonoidK
+
+import scala.language.higherKinds
+
+object CayleyTheorem {
+
+  /* law: abs compose rep == id */
+  type DifferenceList[A] = List[A] => List[A]
+  def rep[A](xs: List[A]): DifferenceList[A] = ys => xs ++ ys
+  def abs[A](xs: DifferenceList[A]): List[A] = xs(Nil)
+
+  /**
+    * Laws:
+    * rep(abs(a)) == a
+    * abs(rep(a)) == a
+    */
+  trait CayleyTheoremForMonoid[M[_]] extends MonoidK[M] {
+    type MonoidEndomorphism[A] = M[A] => M[A]
+    def rep[A](xs: M[A]): MonoidEndomorphism[A] = ys => combineK(xs, ys)
+    def abs[A](xs: MonoidEndomorphism[A]): M[A] = xs(empty)
+  }
+}
