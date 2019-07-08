f30059ab9244ac492d56781a1c377ec756f1fa57
Piotr Paradziński <piotr.paradzinski@scalac.io>
2019-07-08 23:44:02 +0200

    fix broken pattern match in freeNames

--- src/main/scala/typetheory/processcalculi/LambdaCalc.scala
+++ src/main/scala/typetheory/processcalculi/LambdaCalc.scala
@@ -15,7 +15,7 @@ case class Abstraction[X](x: X, m : LambdaCalc[X]) extends LambdaCalc[X]
 case class Application[X](m: LambdaCalc[X], n: LambdaCalc[X]) extends LambdaCalc[X]
 
 object LambdaCalc {
-  def freeNames[X](x: LambdaCalc[X]): Set[X] = {
+  def freeNames[X](x: LambdaCalc[X]): Set[X] = x match {
     case Mention(lx) => Set(lx)
     case Abstraction(lx, lm) => freeNames(lm) - lx
     case Application(m, n) => freeNames(m) ++ freeNames(n)
