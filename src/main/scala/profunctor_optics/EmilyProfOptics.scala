e058f4500caf2e57dd3d04309f56f15f0964d34f
Piotr Paradziński <piotr.paradzinski@scalac.io>
2019-09-10 00:37:22 +0200

    translate optics, peano numbers, optics aliases

--- src/main/scala/profunctor_optics/EmilyProfOptics.scala
+++ src/main/scala/profunctor_optics/EmilyProfOptics.scala
@@ -9,10 +9,9 @@ import scala.language.higherKinds
 
 // TODO https://github.com/mroman42/vitrea/blob/master/Vitrea.hs
 
-// free tambara monad
 // TODO specify monad instance
 // TODO replace List => Traversable
-trait Phi[P[_,_],A,B] {
+trait FreeTambara[P[_,_],A,B] {
   type U
   type V
   type C
@@ -22,12 +21,12 @@ trait Phi[P[_,_],A,B] {
   def _x3: B => (List[V], D)
 }
 
-object Phi {
+object FreeTambara {
   def apply[A, B, CC, DD, UU, VV, P[_, _]](
     x1: A => (List[UU], CC),
     x2: P[UU,VV],
-    x3: B => (List[VV], DD)): Phi[P, A, B] =
-    new Phi[P, A, B] {
+    x3: B => (List[VV], DD)): FreeTambara[P, A, B] =
+    new FreeTambara[P, A, B] {
       type U = UU
       type V = VV
       type C = CC
@@ -38,17 +37,16 @@ object Phi {
     }
 }
 
-trait Theta1[P,A,B]
-
-object Theta1 {
-  def apply[P,A,AA,B,BB, C](t1: (List[AA], C), t2: (List[BB], C)): Theta1[P,A,B] = ???
+trait CofreeTambara[P,A,B] {
+  type C
+  def x1: (List[A],C)
+  def x2: (List[B],C)
 }
 
-trait Theta[P[_,_],A,B]
-
-object Theta {
-  def apply[P[_,_],A,AA,B,BB, C](p: P[(List[AA], C), (List[BB], C)]): Theta[P,A,B] = ???
+object CofreeTambara {
+  def apply[P,A,AA,B,BB, C](t1: (List[AA], C), t2: (List[BB], C)): CofreeTambara[P,A,B] = ???
 }
+
 
 trait Profunctor[P[_,_]] {
   def dimap[S,T,A,B](sa: S => A, bt: B => T): P[A,B] => P[S,T]
@@ -120,4 +118,112 @@ trait Windowed[P[_,_]] extends Choice[P] with Glassed[P] {
 
 trait Polyp[P[_,_]] extends Profunctor[P] {
   def polyper[A,B,F[_]](implicit A: Applicative[F]): P[A,B] => P[F[A],F[B]]
+}
+
+
+object Optics {
+  /*
+  type Optic p s t a b = p a b -> p s t
+
+  type Iso             s t a b = forall p.   Profunctor p => Optic p s t a b
+  type Lens            s t a b = forall p.       Strong p => Optic p s t a b
+  type Prism           s t a b = forall p.       Choice p => Optic p s t a b
+  type Grate           s t a b = forall p.       Closed p => Optic p s t a b
+  type Glass           s t a b = forall p.      Glassed p => Optic p s t a b
+  type AffineTraversal s t a b = forall p.    Magnified p => Optic p s t a b
+  type AffineWindow    s t a b = forall p.   Telescoped p => Optic p s t a b
+  type Window          s t a b = forall p.     Windowed p => Optic p s t a b
+  type Traversal       s t a b = forall p.   Traversing p => Optic p s t a b
+  type Grid            s t a b = forall p.    Polynodal p => Optic p s t a b -- closed traversal
+  type Kaleidescope    s t a b = forall p.        Polyp p => Optic p s t a b
+   */
+
+  type Optics[P[_,_],S,T,A,B] = P[A,B] => P[S,T]
+
+  trait Iso[S,T,A,B] {
+    def apply[P[_,_]]: Profunctor[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Lens[S,T,A,B] {
+    def apply[P[_,_]]: Strong[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Prism[S,T,A,B] {
+    def apply[P[_,_]]: Choice[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Grate[S,T,A,B] {
+    def apply[P[_,_]]: Closed[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Glass[S,T,A,B] {
+    def apply[P[_,_]]: Glassed[P] => Optics[P,S,T,A,B]
+  }
+
+  trait AffineTraversal[S,T,A,B] {
+    def apply[P[_,_]]: Magnified[P] => Optics[P,S,T,A,B]
+  }
+
+  trait AffineWindow[S,T,A,B] {
+    def apply[P[_,_]]: Telescoped[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Window[S,T,A,B] {
+    def apply[P[_,_]]: Windowed[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Traversal[S,T,A,B] {
+    def apply[P[_,_]]: Traversing[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Grid[S,T,A,B] {
+    def apply[P[_,_]]: Polynodal[P] => Optics[P,S,T,A,B]
+  }
+
+  trait Kaleidescope[S,T,A,B] {
+    def apply[P[_,_]]: Polyp[P] => Optics[P,S,T,A,B]
+  }
+
+  /*
+  type Iso' s a = Iso s s a a
+  type Prism' s a = Prism s s a a
+  type Lens' s a = Lens s s a a
+  type Traversal' s a = Traversal s s a a
+  */
+  type Iso1[S,A]       = Iso[S,S,A,A]
+  type Prism1[S,A]     = Prism[S,S,A,A]
+  type Lens1[S,A]      = Lens[S,S,A,A]
+  type Traversal1[S,A] = Traversal[S,S,A,A]
+
+  /*
+  type a ~= b = Iso b a b a
+  type a <= b = Prism' b a
+  type a .| b = Lens' b a
+  type a .& b = Traversal' b a
+  type Log a b = Grate b a b a
+  type Logg a b = Glass b a b a
+  type AutExp b a = Window b a b a
+   */
+  type ~=[A,B] = Iso[B,A,B,A]
+  type <=[A,B] = Prism1[B,A]
+  type *|[A,B] = Lens1[B,A]
+  type *&[A,B] = Traversal1[B,A]
+  type Log[A,B] = Grate[B,A,B,A]
+  type Logg[A,B] = Glass[B,A,B,A]
+  type AutExp[B,A] = Window[B,A,B,A]
+
+  /*
+  type Z = ()
+  type S a = Either a Z
+  type One = S Z
+  type Two = S One
+  type Three = S Two
+  type Four = S Three
+  */
+  type Zero = Unit
+  type Succ[A] = Either[A,Zero]
+  type One = Succ[Zero]
+  type Two = Succ[One]
+  type Three = Succ[Two]
+  type Four = Succ[Three]
 }
