9affe76fc093f158c1a47ed587ba15a7f320bb3a
lemastero <adres.mistrza@gmail.com>
2019-11-29 02:03:36 +0100

    Representable small improvements

--- src/test/scala/comonad/ComonadCustomImplSpec.scala
+++ src/test/scala/comonad/ComonadCustomImplSpec.scala
@@ -1,6 +1,6 @@
 package comonad
 
-import functor.FunctorSimpleImpl.Functor
+import educational.Functor
 import simple.Id
 import org.scalatest.{FunSpec, MustMatchers}
 import semigroup.MonoidSimpleImpl.Monoid
