b09ee8ba629145f042ff487db175db4b4dc3f794
lemastero <adres.mistrza@gmail.com>
2020-01-14 01:50:34 +0100

    add better encoding of monidal category, braided monoidal category and braided monoidal category

--- src/main/scala/educational/category_theory/higher/monoidal/MonoidalCategory.scala
+++ src/main/scala/educational/category_theory/higher/monoidal/MonoidalCategory.scala
@@ -1,5 +1,7 @@
 package educational.category_theory.higher.monoidal
 
+import educational.category_theory.Category
+import educational.category_theory.CategoryInstances.Function1Cat
 import educational.category_theory.two.Bifunctor
 
 /**
@@ -10,67 +12,83 @@ import educational.category_theory.two.Bifunctor
   */
 object MonoidalCategory {
 
-  trait MonoidalCategory[M[_, _], I] {
-    val mcBif: Bifunctor[M]
-    val mcId: I
+  trait MonoidalCategory[:=>[_,_],⊗[_, _], I]
+    extends Category[:=>] {
 
-    def rho[A]    (fa: M[A,I]): A
-    def rho_inv[A](a:  A):      M[A, I]
+    def tensor: Bifunctor[⊗]
+    def ρ[A]    (fa: A⊗I): A
+    def ρ_inv[A](a:  A):   A⊗I
 
-    def lambda[A]      (fa: M[I,A]): A
-    def lambda_inv[A,B](a:  A):      M[I, A]
+    def λ[A]      (fa: I⊗A): A
+    def λ_inv[A,B](a:  A):   I⊗A
 
-    def alpha[A,B,C](    fa: M[M[A,B], C]): M[A, M[B,C]]
-    def alpha_inv[A,B,C](fa: M[A, M[B,C]]): M[M[A,B], C]
+    def α[A,B,C](    fa: (A⊗B)⊗C): A⊗(B⊗C)
+    def α_inv[A,B,C](fa: A⊗(B⊗C)): (A⊗B)⊗C
   }
 
-  trait MonoidalCategoryLaws[M[_, _], I] extends MonoidalCategory[M,I] {
+  trait MonoidalCategoryLaws[:=>[_,_],⊗[_, _], I]
+    extends MonoidalCategory[:=>,⊗, I] {
 
-    def triangleEquations[A, B](fa: M[M[A, I], B]): Boolean = {
-      //               ro[A] * id[B]
-      // (A * I) * C ----------------> A * B
-      val v1: M[A, B] = mcBif.bimap(rho[A], identity[B])(fa)
+    def triangleEquations[A, B](fa: (A⊗I)⊗B): Boolean = {
+      //               ρ[A] ⊗ id[B]
+      // (A ⊗ I) ⊗ B ----------------> A ⊗ B
+      val v1: A⊗B = tensor.bimap(ρ[A],identity[B])(fa)
 
-      //              alpha[A,I,B]                  id[A] * lambda[B]
-      // (A * I) * C -------------->  A * (I * C) --------------------> A * B
-      val w1: M[A, M[I, B]] = alpha[A,I,B](fa)
-      val w2: M[A, B] = mcBif.bimap[A,A, M[I,B],B](identity[A],lambda[B])(w1)
+      //              α[A,I,B]
+      // (A ⊗ I) ⊗ B ---------->  A ⊗ (I ⊗ B)
+      val w1: A⊗(I⊗B) = α[A,I,B](fa)
+
+      //               id[A] ⊗ λ[B]
+      // A ⊗ (I ⊗ B) ---------------> A ⊗ B
+      val w2: A⊗B = tensor.bimap(identity[A],λ[B])(w1)
 
       v1 == w2
     }
 
-    def pentagonEquations[A,B,C,D](fa: M[M[M[A,B],C],D]): Boolean = {
-      //                    alpha[A,B,C] * 1D                      alpha[A,B*C,D]
-      // ((A * B) * C) * D -------------------> (A * (B * C)) * D ------------------> A * ((B * C) * D)
-      //                    1A * alpha[B,C,D]
-      // A * ((B * C) * D) ------------------> A * (B * (C * D))
-      val v1: M[M[A, M[B, C]], D] = mcBif.bimap(alpha[A,B,C],identity[D])(fa)
-      val v2: M[A, M[M[B,C], D]] = alpha[A,M[B,C],D](v1)
-      val v3: M[A, M[B, M[C,D]]] = mcBif.bimap(identity[A],alpha[B,C,D])(v2)
+    def pentagonEquations[A,B,C,D](fa: ((A⊗B)⊗C)⊗D): Boolean = {
+      //                    α[A,B,C] ⊗ 1D
+      // ((A ⊗ B) ⊗ C) ⊗ D ---------------> (A ⊗ (B ⊗ C)) ⊗ D
+      val v1: (A⊗(B⊗C))⊗D = tensor.bimap(α[A,B,C],identity[D])(fa)
 
-      //                     alpha[A*B,C,D]                       alpha[A,B,C*D]
-      // ((A * B) * C) * D -----------------> (A * B) * (C * D) -----------------> A * (B * (C * D))
-      val w1: M[M[A,B], M[C,D]] = alpha[M[A,B],C,D](fa)
-      val w2: M[A,M[B,M[C,D]]] = alpha[A,B,M[C,D]](w1)
+      //                    α[A,B⊗C,D]
+      // (A ⊗ (B ⊗ C)) ⊗ D ------------> A ⊗ ((B ⊗ C) ⊗ D)
+      val v2: A⊗((B⊗C)⊗D) = α[A,B⊗C,D](v1)
+
+      //                    1A ⊗ α[B,C,D]
+      // A ⊗ ((B ⊗ C) ⊗ D) ---------------> A ⊗ (B ⊗ (C ⊗ D))
+      val v3: A⊗(B⊗(C⊗D)) = tensor.bimap(identity[A],α[B,C,D])(v2)
+
+      //                     α[A⊗B,C,D]
+      // ((A ⊗ B) ⊗ C) ⊗ D -------------> (A ⊗ B) ⊗ (C ⊗ D)
+      val w1: (A⊗B)⊗(C⊗D) = α[A⊗B,C,D](fa)
+
+      //                      α[A,B,C⊗D]
+      // (A ⊗ B) ⊗ (C ⊗ D) -------------> A ⊗ (B ⊗ (C ⊗ D))
+      val w2: A⊗(B⊗(C⊗D)) = α[A,B,C⊗D](w1)
 
       v3 == w2
     }
   }
 
-  val tupleBifunctor: Bifunctor[Tuple2] = new Bifunctor[Tuple2] {
-    override def bimap[A, B, C, D](f: A => B, g: C => D): Tuple2[A, C] => Tuple2[B, D] = { case (a,c) => (f(a), g(c)) }
+val tupleBifunctor: Bifunctor[Tuple2] = new Bifunctor[Tuple2] {
+  override def bimap[A,B,C,D](f: A => B, g: C => D): Tuple2[A,C] => Tuple2[B,D] =
+    { case (a,c) => (f(a), g(c)) }
+}
+
+  trait TupleMc
+    extends MonoidalCategory[Function1,Tuple2,Unit]
+    with Function1Cat {
+
+    val tensor: Bifunctor[Tuple2] = tupleBifunctor
+    def ρ[A](fa: (A, Unit)): A = fa._1
+    def ρ_inv[A](a: A): (A, Unit) = (a, ())
+    def λ[A](fa: (Unit, A)): A = fa._2
+    def λ_inv[A, B](a: A): (Unit, A) = ((), a)
+    def α[A, B, C](fa: ((A, B), C)): (A, (B, C)) = fa match {case ((a,b),c) => (a, (b, c)) }
+    def α_inv[A, B, C](fa: (A, (B, C))): ((A, B), C) = fa match {case (a,(b,c)) => ((a, b), c) }
   }
 
-  val productMonoidalCategory: MonoidalCategory[Tuple2, Unit] = new MonoidalCategory[Tuple2, Unit] {
-    val mcBif: Bifunctor[Tuple2] = tupleBifunctor
-    val mcId: Unit = ()
-    def rho[A](fa: (A, Unit)): A = fa._1
-    def rho_inv[A](a: A): (A, Unit) = (a, mcId)
-    def lambda[A](fa: (Unit, A)): A = fa._2
-    def lambda_inv[A, B](a: A): (Unit, A) = (mcId, a)
-    def alpha[A, B, C](fa: ((A, B), C)): (A, (B, C)) = fa match {case ((a,b),c) => (a, (b, c)) }
-    def alpha_inv[A, B, C](fa: (A, (B, C))): ((A, B), C) = fa match {case (a,(b,c)) => ((a, b), c) }
-  }
+  val productMonoidalCategory: MonoidalCategory[Function1,Tuple2,Unit] = new TupleMc{}
 
   type Void <: Nothing
 
@@ -81,48 +99,83 @@ object MonoidalCategory {
     }
   }
 
-  val coproductMonoidalCategory: MonoidalCategory[Either, Void] = new MonoidalCategory[Either, Void] {
-    val mcBif: Bifunctor[Either] = eitherBifunctor
-    val mcId: Void = throw new RuntimeException("This exception was not thrown!")
-    def rho[A](fa: Either[A, Void]): A = fa match { case Left(a) => a }
-    def rho_inv[A](a: A): Either[A, Void] = Left(a)
-    def lambda[A](fa: Either[Void, A]): A = fa match { case Right(a) => a }
-    def lambda_inv[A, B](a: A): Either[Void, A] = Right(a)
-    def alpha[A, B, C](fa: Either[Either[A, B], C]): Either[A, Either[B, C]] =
+  trait Function1EitherMc
+    extends MonoidalCategory[Function1,Either,Void]
+      with Function1Cat {
+
+    val tensor: Bifunctor[Either] = eitherBifunctor
+    def ρ[A](fa: Either[A, Void]): A = fa match { case Left(a) => a }
+    def ρ_inv[A](a: A): Either[A, Void] = Left(a)
+    def λ[A](fa: Either[Void, A]): A = fa match { case Right(a) => a }
+    def λ_inv[A, B](a: A): Either[Void, A] = Right(a)
+    def α[A, B, C](fa: Either[Either[A, B], C]): Either[A, Either[B, C]] =
       fa match {
         case Left(Left(a)) => Left(a)
         case Left(Right(b)) => Right(Left(b))
         case Right(c) => Right(Right(c))
       }
-    def alpha_inv[A, B, C](fa: Either[A, Either[B, C]]): Either[Either[A, B], C] = fa match {
+    def α_inv[A, B, C](fa: Either[A, Either[B, C]]): Either[Either[A, B], C] = fa match {
       case Left(a) => Left(Left(a))
       case Right(Left(b)) => Left(Right(b))
       case Right(Right(c)) => Right(c)
     }
   }
 
+  val coproductMonoidalCategory: MonoidalCategory[Function1,Either,Void] = new Function1EitherMc {}
 
-  trait BraidedMonoidalCategory[M[_,_], I] extends MonoidalCategory[M, I] {
-    def braiding[A,B](a: M[A,B]): M[B,A]
+  trait BraidedMonoidalCategory[:=>[_,_],⊗[_, _], I]
+    extends MonoidalCategory[:=>,⊗, I] {
+
+    def braiding[A,B](a: A⊗B): B⊗A
   }
 
-//  trait BraidedMonoidalCategoryLaws[M[_, _], I] extends MonoidalCategory[M,I] with MonoidalCategoryLaws[M, I] {
-//
-//    def hexagonEquations[A,B,C,D](fa: M[M[M[A,B],C],D]): Boolean = {
-//      //                    alpha[A,B,C] * 1D                      alpha[A,B*C,D]
-//      // ((A * B) * C) * D -------------------> (A * (B * C)) * D ------------------> A * ((B * C) * D)
-//      //                    1A * alpha[B,C,D]
-//      // A * ((B * C) * D) ------------------> A * (B * (C * D))
-//      val v1: M[M[A, M[B, C]], D] = mcBif.bimap(alpha[A,B,C],identity[D])(fa)
-//      val v2: M[A, M[M[B,C], D]] = alpha[A,M[B,C],D](v1)
-//      val v3: M[A, M[B, M[C,D]]] = mcBif.bimap(identity[A],alpha[B,C,D])(v2)
-//
-//      //                     alpha[A*B,C,D]                       alpha[A,B,C*D]
-//      // ((A * B) * C) * D -----------------> (A * B) * (C * D) -----------------> A * (B * (C * D))
-//      val w1: M[M[A,B], M[C,D]] = alpha[M[A,B],C,D](fa)
-//      val w2: M[A,M[B,M[C,D]]] = alpha[A,B,M[C,D]](w1)
-//
-//      v3 == w2
-//    }
-//  }
+  trait BraidedMonoidalCategoryLaws[:=>[_,_],⊗[_, _], I]
+    extends BraidedMonoidalCategory[:=>,⊗, I]
+      with MonoidalCategoryLaws[:=>,⊗, I] {
+
+    def hexagonEquations[A,B,C,D](fa: ((A⊗B)⊗C)⊗D): Boolean = {
+      //                      α[A,B,C] ⊗ 1D
+      // ((A ⊗ B) ⊗ C) ⊗ D -----------------> (A ⊗ (B ⊗ C)) ⊗ D
+      val v1: (A⊗(B⊗C))⊗D = tensor.bimap(α[A,B,C],identity[D])(fa)
+      //                     α[A,B⊗C,D]
+      // (A ⊗ (B ⊗ C)) ⊗ D ------------> A ⊗ ((B ⊗ C) ⊗ D)
+      val v2: A⊗((B⊗C)⊗D) = α[A,B⊗C,D](v1)
+      //                    1A ⊗ α[B,C,D]
+      // A ⊗ ((B ⊗ C) ⊗ D) ------------------> A ⊗ (B ⊗ (C ⊗ D))
+      val v3: A⊗(B⊗(C⊗D)) = tensor.bimap(identity[A],α[B,C,D])(v2)
+
+      //                     α[A⊗B,C,D]
+      // ((A ⊗ B) ⊗ C) ⊗ D -------------> (A ⊗ B) ⊗ (C ⊗ D)
+      val w1: ((A⊗B)⊗(C⊗D)) = α[A⊗B,C,D](fa)
+      //                     α[A,B,C⊗D]
+      // (A ⊗ B) ⊗ (C ⊗ D) ------------> A ⊗ (B ⊗ (C ⊗ D))
+      val w2: A⊗(B⊗(C⊗D)) = α[A,B,C⊗D](w1)
+
+      v3 == w2
+    }
+  }
+
+  val productBraidedMonoidalCategory: BraidedMonoidalCategory[Function1,Either,Void] =
+    new BraidedMonoidalCategory[Function1,Either,Void]
+      with Function1EitherMc {
+
+      def braiding[A,B](a: Either[A,B]): Either[B,A] = a.swap
+    }
+
+  val tupleBraidedMonoidalCategory: BraidedMonoidalCategory[Function1,Tuple2,Unit] =
+    new BraidedMonoidalCategory[Function1,Tuple2,Unit]
+      with TupleMc {
+
+      def braiding[A,B](a: (A,B)): (B,A) = a.swap
+    }
+
+
+  trait SymmetricMonoidalCategoryLaws[:=>[_,_],⊗[_, _], I]
+    extends BraidedMonoidalCategory[:=>,⊗, I]
+      with BraidedMonoidalCategoryLaws[:=>,⊗, I] {
+
+    def hexagonEquations[A,B](fa: A⊗B): Boolean = {
+      braiding(braiding(fa)) == fa
+    }
+  }
 }
