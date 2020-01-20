a29754783994cdf2b88d059cb563913123567668
lemastero <adres.mistrza@gmail.com>
2020-01-20 09:27:06 +0100

    fix est for strong profunctor

--- src/test/scala/educational/category_theory/two/profunctor/StrongSpec.scala
+++ src/test/scala/educational/category_theory/two/profunctor/StrongSpec.scala
@@ -7,8 +7,8 @@ import org.scalatest.matchers.must.Matchers
 case class DoubleFun[X,Y](fun: (X,X) => (Y,Y))
 
 trait DoubleFunPro extends Profunctor[DoubleFun] {
-  def dimap[X, Y, Z, W](f: X => Y, g: Z => W): DoubleFun[Y, Z] => DoubleFun[X, W] = (fa: DoubleFun[Y, Z]) =>
-    DoubleFun[X, W] { (c1, c2) => fa.fun(f(c1), f(c2)) match {case (z1, z2) => (g(z1), g(z2)) } }
+  override def dimap[S, T, A, B](pab: DoubleFun[A, B])(f: S => A, g: B => T): DoubleFun[S, T] =
+    DoubleFun[S, T] { (c1, c2) => pab.fun(f(c1), f(c2)) match {case (z1, z2) => (g(z1), g(z2)) } }
 }
 
 class StrongSpec
@@ -17,7 +17,7 @@ class StrongSpec
 
   it("Functions from A to B are Strong Profunctors") {
     trait Function1Profunctor extends Profunctor[Function1] {
-      def dimap[X, Y, Z, W](ab: X => Y, cd: Z => W): (Y => Z) => X => W = fyz => ab andThen fyz andThen cd
+      override def dimap[S,T,A,B](pab: A => B)(ab: S => A, cd: B => T): S => T = ab andThen pab andThen cd
     }
 
     val functionPro: Profunctor[Function1] = new Function1Profunctor {}
@@ -33,7 +33,7 @@ class StrongSpec
 
   it("You can define multiple Strong Profunctors for P[_,_]") {
     val doubleFunProStrength: Strong[DoubleFun] with DoubleFunPro = new Strong[DoubleFun] with DoubleFunPro {
-      def first[A, B, C](fa: DoubleFun[A, B]): DoubleFun[(A, C), (B, C)] = DoubleFun {
+      override def first[A, B, C](fa: DoubleFun[A, B]): DoubleFun[(A, C), (B, C)] = DoubleFun {
         (a1: (A, C), a2: (A, C)) =>
           val bb = fa.fun(a1._1, a2._1)
           ((bb._1, a1._2), (bb._2, a2._2))
@@ -41,7 +41,7 @@ class StrongSpec
     }
 
     val doubleFunProStrength2: Strong[DoubleFun] with DoubleFunPro = new Strong[DoubleFun] with DoubleFunPro {
-      def first[A, B, C](fa: DoubleFun[A, B]): DoubleFun[(A, C), (B, C)] = DoubleFun {
+      override def first[A, B, C](fa: DoubleFun[A, B]): DoubleFun[(A, C), (B, C)] = DoubleFun {
         (a1: (A, C), a2: (A, C)) =>
           val bb = fa.fun(a1._1, a2._1)
           ((bb._1, a1._2), (bb._1, a2._2))
