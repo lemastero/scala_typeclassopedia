85e87e0a851b97dfd8563e53cbe064dc87e8071c
lemastero <adres.mistrza@gmail.com>
2020-07-23 15:47:34 +0200

    add closed profunctor instances and WIP closed profunctor laws

--- src/main/scala/educational/category_theory/two/profunctor/closed/Closed.scala
+++ src/main/scala/educational/category_theory/two/profunctor/closed/Closed.scala
@@ -1,7 +1,16 @@
 package educational.category_theory.two.profunctor.closed
 
 import educational.category_theory.two.profunctor.Profunctor
+import educational.category_theory.two.profunctor.ProfunctorLaws
+import educational.category_theory.two.profunctor.ProfunctorInstance.Function1Profunctor
+import Function.const
+import Function.uncurried
+import Function.untupled
 
+// https://ncatlab.org/nlab/show/closed+category
+
+// class Profunctor p => Closed p where
+//    closed :: p a b -> p (x -> a) (x -> b)
 /**
   * Laws:
   *
@@ -14,5 +23,16 @@ trait Closed[=>:[_,_]] extends Profunctor[=>:] {
 }
 
 object ClosedInstances { // https://hackage.haskell.org/package/profunctors-5.3/docs/Data-Profunctor-Closed.html#i:Closed
-  // TODO
+  val Function1Closed: Closed[Function1] = new Closed[Function1] with Function1Profunctor {
+    override def closed[A,B,C](f: A => B): (C => A) => C => B = _ andThen f
+  }
+}
+
+trait ClosedLaws[=:>[_,_]] extends Closed[=:>] with ProfunctorLaws[=:>] {
+  // lmap (. f) . closed ≡ rmap (. f) . closed
+  def lmapClosedEqRmapClosed[A, B, C, D](p: A =:> B, f: (D => A) => (C => A), g: (D => B) => (C => B)): Boolean = {
+    val l1: (C => A) =:> (C => B) = closed(p)
+    val l2: (D => A) =:> (C => B) = lmap(l1)(f)
+    ???
+  }
 }
