430c14dc7269e6898472d758b14a309a4c5aa1e3
lemastero <adres.mistrza@gmail.com>
2020-11-18 21:00:01 +0100

    Bifunctor => Bicovariant

--- src/test/scala/bifunctor/BicovariantExamplesSpec.scala
+++ src/test/scala/bifunctor/BicovariantExamplesSpec.scala
@@ -2,22 +2,19 @@ package bifunctor
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.must.Matchers
+import zio.prelude._
 
-class BifunctorExamplesSpec extends AnyFunSpec with Matchers {
+class BicovariantExamplesSpec extends AnyFunSpec with Matchers {
 
-  describe("Bifunctor") {
+  describe("Bicovariant") {
     describe("bimap") {
       it("apply given functions for first and second element of tuple") {
-        import cats.syntax.all._
-        (List("foo", "bar"), 42).bimap(_.headOption, _ - 1) mustBe (Some(
-          "foo"
-        ), 41)
+        (List("foo", "bar"), 42).bimap(_.headOption, _ - 1) mustBe (Some("foo"), 41)
       }
     }
 
     describe("leftmap") {
       it("apply given functions for first and second element of tuple") {
-        import cats.syntax.all._
         (List("foo", "bar"), 42).leftMap(_.headOption) mustBe (Some("foo"), 42)
       }
     }
